e28ebc9929514c2c01cad643868c99a735d5e095
Vitor Dal Pra <vitordalpra@gmail.com>
2020-11-19 15:00:35 -0300

    doses mvc done, backend done

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,5 +3,5 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
 
   validates :description, :cocktail, :ingredient, presence: true
-  validates :cocktail, uniqueness: { scope: :ingredient }
+  validates :ingredient, uniqueness: { scope: :cocktail }
 end
